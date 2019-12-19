04368157a5d3d2e949bd2fd3193f9398fa6b3e0d
BIT <BIT@DESKTOP-S4KDBK1>
2019-12-19 10:53:16 +0900

    init commit

---
+++ src/com/bigdata2019/helloweb/controller/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.bigdata2019.helloweb.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(
+		HttpServletRequest request,
+		HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.print("<h1>Hello World</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
